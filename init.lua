3bbfed92d77863ceab6fab5cde2ce7251434ee74
gc1644 <gleb@duck.com>
2026-02-02 17:15:25 +0300

    initial

---
+++ init.lua
@@ -0,0 +1,11 @@
+Luavim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.number         = true
+vim.opt.relativenumber = true
+vim.opt.clipboard      = "unnamedplus"
+vim.opt.termguicolors  = true
+vim.opt.showmode       = false
+vim.opt.cursorline     = true
+vim.opt.signcolumn     = "yes"
+vim.opt.mouse          = "a"
