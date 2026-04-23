d987e63953b4dcaeed11eaf52f5687a4cebc8cda
gc1644 <gleb@duck.com>
2026-04-23 08:34:32 +0300

    catppuccin

--- init.lua
+++ init.lua
@@ -8,32 +8,58 @@ vim.opt.cursorline = true
 vim.opt.signcolumn = "yes"
 
 vim.pack.add({
-  "https://github.com/vague-theme/vague.nvim",
-  "https://github.com/nvim-mini/mini.files",
+  { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+  { src = "https://github.com/nvim-telescope/telescope.nvim" },
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+  { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 })
 
-require("vague").setup({
-  transparent = false,
-  italic = false,
-})
-vim.cmd.colorscheme("vague")
-
-require("mini.files").setup({
-  windows = {
-    preview = false,
-    width_focus = 30,
-    width_nofocus = 15,
+require("catppuccin").setup({
+  flavour = "auto",
+  background = {
+    light = "latte",
+    dark = "mocha",
   },
-  mappings = {
-    go_in = 'l',
-    go_in_plus = '<CR>',
-    go_out = 'h',
-    go_out_plus = 'H',
+  transparent_background = false,
+  float = {
+    transparent = false,
+    solid = false,
   },
+  term_colors = false,
+  dim_inactive = {
+    enabled = false,
+    shade = "dark",
+    percentage = 0.15,
+  },
+  no_italic = false,
+  no_bold = false,
+  no_underline = false,
 })
 
-vim.keymap.set("n", "<leader>e", function()
-  local file = vim.api.nvim_buf_get_name(0)
-  if file == "" then file = vim.fn.getcwd() end
-  require("mini.files").open(file)
-end, { desc = "Open mini.files" })
+vim.cmd.colorscheme("catppuccin")
+
+local telescope = require("telescope")
+
+telescope.setup({
+  defaults = {
+    file_ignore_patterns = { "node_modules", ".git/" },
+  },
+  extensions = {
+    fzf = {}
+  }
+})
+
+pcall(telescope.load_extension, "fzf")
+
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep text" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help" })
+vim.keymap.set("n", "<leader>e", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fn", function()
+  require("telescope.builtin").find_files({
+    cwd = vim.fn.expand("~/.config"),
+  })
+end, { desc = "Find in ~/.config" })
